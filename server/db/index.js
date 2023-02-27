8b24ee5fd77c2c224db276c73252c2afdd5c3488
Lai Bazemore <saech@Lais-MacBook-Pro.local>
2023-02-27 16:22:07 -0500

    changed association

--- server/db/index.js
+++ server/db/index.js
@@ -4,17 +4,20 @@ const Order = require('./order');
 const Movie = require('./movie');
 const Personnel = require('./personnel')
 
-User.hasMany(Order);
-Order.belongsTo(User);
-Personnel.hasMany(Movie);
-Movie.hasMany(Personnel);
-Order.hasMany(Movie);
-Movie.hasMany(Order);
+// User.hasMany(Order);
+// Order.belongsTo(User);
+// Personnel.hasMany(Movie);
+// Movie.hasMany(Personnel);
+// Order.hasMany(Movie);
+// Movie.hasMany(Order);
+
+Personnel.belongsToMany(Movie, {through: 'MoviePersonnel'});
+Movie.belongsToMany(Personnel, {through: 'MoviePersonnel'});
 
 module.exports = {
 	db,
 	User,
 	Order,
-	Movie, 
+	Movie,
 	Personnel
 }
