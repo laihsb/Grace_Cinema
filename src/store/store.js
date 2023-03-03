adb22083e6f541f12b83096ddda5a55aded55886
ronalbasnet <39981871+ronalbasnet@users.noreply.github.com>
2023-03-03 03:15:24 -0800

    The functionality in the cart is working also had to touch a lot of other files

--- src/store/store.js
+++ src/store/store.js
@@ -3,15 +3,17 @@ import usersSlice from "../features/users";
 import allMoviesSlice from "../features/allMovies/allMoviesSlice";
 import ordersSlice from "../features/orders";
 import allPersonnelSlice from "../features/allPersonnelSlice";
-
+import cartReducer, { getTotals } from "../features/cartSlice";
 
 const store = configureStore({
   reducer: {
     users: usersSlice,
-		movies: allMoviesSlice,
+    movies: allMoviesSlice,
     orders: ordersSlice,
-		personnel: allPersonnelSlice,
+    personnel: allPersonnelSlice,
+    cart: cartReducer,
   },
 });
-
+// First dispatch when the application loads
+store.dispatch(getTotals());
 export default store;
