ef2a367b617cf8bf800e0836c4f24afd2eb484d8
Lai Bazemore <saech@Lais-MacBook-Pro.local>
2023-03-05 20:55:47 -0500

    updated json package to add webpack should be able to deploy

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const toastify = require('react-toastify')
+module.exports = {
+  mode: "production",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+},
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+  },
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader'],
+  },
+],
+}
+}
