ccfe5793315fcb8fd1afba694d3f61cc9d5a15dd
Lai Bazemore <saech@Lais-MacBook-Pro.local>
2023-03-06 15:04:00 -0500

    removed seed

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -12,6 +12,70 @@ import Orders from "./Orders";
 import SideNav from "./SideNav";
 import NoPage from "./NoPage";
 
+{/* <script type="text/babel">
+const SignIn = ({ signIn }) => {
+	const [username, setUsername] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	const onChange = (ev) => {
+		if (ev.target.name === 'username') setUsername(ev.target.value);
+		if (ev.target.name === 'password') setPassword(ev.target.value);
+	};
+	const onSubmit = (ev) => {
+		ev.preventDefault();
+		signIn({ username, password });
+	};
+	return (
+		<form onSubmit={onSubmit}>
+			<input value={username} onChange={onChange} name="username" />
+			<input value={password} onChange={onChange} name="password" />
+			<button>Sign In</button>
+		</form>
+	);
+};
+
+const App = () => {
+	const [auth, setAuth] = React.useState({});
+
+	const attemptTokenLogin = async () => {
+		const token = window.localStorage.getItem('token');
+		if (token) {
+			const { data: auth } = await axios.get('/api/auth', {
+				headers: {
+					authorization: token,
+				},
+			});
+			setAuth(auth);
+		}
+	};
+	React.useEffect(() => {
+		attemptTokenLogin();
+	}, []);
+
+	const logout = () => {
+		window.localStorage.removeItem('token');
+		setAuth({});
+	};
+
+	const signIn = async (credentials) => {
+		const response = await axios.post('/api/auth', credentials);
+		const { token } = response.data;
+		window.localStorage.setItem('token', token);
+		attemptTokenLogin();
+	};
+
+	if (!auth.id) return <SignIn signIn={signIn} />;
+
+	return (
+		<div>
+			Welcome {auth.username}!
+			<button onClick={logout}>Logout</button>
+		</div>
+	);
+};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+</script> */}
+
 const LogIn = () => {
 	return (
 		<h1>Welcome to the LogIn Component!</h1>
